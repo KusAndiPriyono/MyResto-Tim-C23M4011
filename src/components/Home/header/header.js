70b89e87ebb239402c8c1227b9d8a13ecd65ab08
KusAndiPriyono <andipriyono94@gmail.com>
2023-05-17 11:49:29 +0700

    Update package framework MUI

--- src/components/Home/header/header.js
+++ src/components/Home/header/header.js
@@ -1,28 +1,125 @@
-import React from 'react';
-import './header.scss';
+import * as React from 'react';
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  //   Box,
+  //   Toolbar,
+  //   IconButton,
+  //   Typography,
+  //   Menu,
+  //   Container,
+  //   Avatar,
+  //   Button,
+  //   Tooltip,
+  //   MenuItem,
+} from '@mui/material';
 
-export default function Header() {
+import MenuIcon from '@mui/icons-material/Menu';
+import RestoIcon from '@mui/icons-material/Restaurant';
+
+const pages = ['Home', 'Menu', 'Booking Resto', 'Contact Us'];
+const settings = ['Profile', 'My account', 'Logout'];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <header class='header' id='header'>
-      <div class='navbar-wrapper'>
-        <nav class='navbar'>
-          <img
-            class='logo'
-            src='/images/logo-resto.jpg'
-            alt='logo-resto'
-            height='70'
-          />
-          <button class='hamburger-btn' id='hamburger-btn'>
-            <i class='fas fa-bars'></i>
-          </button>
-          <ul class='nav-items'>
-            <li class='nav-item'>Home</li>
-            <li class='nav-item'>Menu</li>
-            <li class='nav-item'>Booking Resto</li>
-            <li class='nav-item'>Contact Us</li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <AppBar position='static' color='default'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <RestoIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant='h6'
+            noWrap
+            component='a'
+            href='/'
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            My Resto
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <RestoIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant='h5'
+            noWrap
+            component='a'
+            href=''
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            My Resto
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
+
+export default Header;
