8de0b4f6214c5800cd35940d4432594bd033a5cc
Nirwan102 <nirwan@raharja.info>
2023-06-13 18:26:39 +0700

    Update Layout Review

--- frontend/src/components/templates/ReviewsLayout.tsx
+++ frontend/src/components/templates/ReviewsLayout.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from 'react';
 import * as API from 'api/services';
 import { useParams } from 'react-router-dom';
-import { Card } from '@mui/material';
+import {
+  Avatar,
+  Card,
+  Container,
+  Grid,
+  Rating,
+  Typography,
+} from '@mui/material';
 
 import React from 'react';
 // import Slider from 'react-slick';
@@ -22,24 +29,24 @@ export default function ReviewsLayout(props: Props) {
     dots: true,
     infinite: false,
     speed: 400,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 2,
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1180,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 850,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
@@ -71,25 +78,69 @@ export default function ReviewsLayout(props: Props) {
   }, [id, name, reviews]);
 
   return (
-    <Slider {...settings}>
-      {reviews.map((review) => (
-        <div key={review.id}>
-          <Card
-            sx={{
-              backgroundColor: '#ffffff',
-              padding: '50px',
-              borderRadius: '8px',
-              boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-              marginBottom: '10px',
-              textAlign: 'center',
-            }}
-          >
-            <p>{review.user.name}</p>
-            <p>{review.review}</p>
-            <p>{review.rating}</p>
-          </Card>
-        </div>
-      ))}
-    </Slider>
+    <Container sx={{ mt: 10, mb: 10 }}>
+      <Slider {...settings}>
+        {reviews.map((review) => (
+          <div key={review.id}>
+            <Grid
+              container
+              wrap='wrap'
+              spacing={2}
+              gap={2}
+              justifyContent='center'
+              columns={{ xs: 4, sm: 8, md: 6 }}
+            >
+              <Card
+                sx={{
+                  maxWidth: 1200,
+                  height: 150,
+                  width: 200,
+                  backgroundColor: '#ffffff',
+                  padding: '50px',
+                  boxShadow: 1,
+                  marginBottom: '10px',
+                  borderRadius: '8px',
+                  transition: 'background-color 0.3s',
+                  '&:hover': {
+                    backgroundColor: '#c2c0c0',
+                  },
+                }}
+              >
+                <Typography
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    marginBottom: '10px',
+                    gap: 2,
+                  }}
+                >
+                  <Avatar src='https://icon-icons.com/id/icon/bulat-akun-tombol-dengan-pengguna-dalam/72596' />
+                  {review.user.name.charAt(0).toUpperCase() +
+                    review.user.name.slice(1)}
+                </Typography>
+                <Typography>
+                  <p>{review.review}</p>
+                  <Typography
+                    variant='body2'
+                    color='#000000'
+                    sx={{ display: 'flex'}}
+                  >
+                    &nbsp;
+                    <Rating
+                      size='small'
+                      name='half-rating-read'
+                      value={review.rating}
+                      precision={0.1}
+                      readOnly
+                    />
+                    &nbsp; ({review.rating})
+                  </Typography>
+                </Typography>
+              </Card>
+            </Grid>
+          </div>
+        ))}
+      </Slider>
+    </Container>
   );
 }
